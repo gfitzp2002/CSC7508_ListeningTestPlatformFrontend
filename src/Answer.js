1126f5c5a5918d1b8db071817aff0c58826aa2d8
gitzp2002 <44789358+gfitzp2002@users.noreply.github.com>
2023-11-21 13:42:57 +0000

    Developed question and answer components - browser currently displays is submitted answer is correct

---
+++ src/Answer.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import './Answer.css';
+
+function Answer({ answers, correctAnswer, onSubmission }) {
+  const [selectedAnswer, setSelectedAnswer] = useState('');
+
+  const handleChange = (event) => {
+    setSelectedAnswer(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    // Check if the selected answer is correct and call the callback function
+    const isCorrect = selectedAnswer === correctAnswer.answerText;
+    onSubmission(isCorrect);
+  };
+
+  return (
+    <div>
+      <h3>Possible Answers:</h3>
+      <ul id='answers'>
+        {answers.map((answer) => (
+          <li key={answer.answerId}>
+            <label>
+              <input
+                type="radio"
+                value={answer.answerText}
+                checked={selectedAnswer === answer.answerText}
+                onChange={handleChange}
+              />
+              <h5>{answer.answerText}</h5>
+            </label>
+          </li>
+        ))}
+      </ul>
+      <button className='btn btn-danger' onClick={handleSubmit}>Submit</button>
+    </div>
+  );
+}
+
+export default Answer;
